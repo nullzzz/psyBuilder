63fbf3b429a7ea6438c424e308d7f702d668917d
nullzzz <903275628@qq.com>
2018-08-13 14:51:49 +0800

    更新video若干细节

--- Center/EventTabs/Video/videoDisplay.py
+++ Center/EventTabs/Video/videoDisplay.py
@@ -8,17 +8,18 @@ from .videoProperty import VideoProperty
 
 class VideoDisplay(QMainWindow):
     propertiesChanged = pyqtSignal(dict)
+
     def __init__(self, parent=None):
         super(VideoDisplay, self).__init__(parent)
 
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        videoWidget = QVideoWidget()
-        self.mediaPlayer.setVideoOutput(videoWidget)
-        self.setCentralWidget(videoWidget)
+        self.videoWidget = QVideoWidget()
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
 
         self.label = QLabel()
         self.pro = VideoProperty()
         self.pro.ok_bt.clicked.connect(self.ok)
+        self.pro.cancel_bt.clicked.connect(self.pro.close)
         self.pro.apply_bt.clicked.connect(self.apply)
 
         self.file = ""
@@ -30,8 +31,8 @@ class VideoDisplay(QMainWindow):
         self.stop_after_mode = "OffsetTime"
         self.stretch = False
         self.stretch_mode = "Both"
-        self.end_video_action = "none"
-        self.display_name = "Display"
+        # self.end_video_action = "none"
+        self.screen_name = "Display"
         self.clear_after = "Yes"
 
         self.x_pos = 0
@@ -52,7 +53,7 @@ class VideoDisplay(QMainWindow):
         open_pro.triggered.connect(self.open_pro)
         tool.addAction(open_pro)
 
-        play_video = QAction(QIcon(".\\.\\image\\preview"), "view", self)
+        play_video = QAction(QIcon(".\\.\\image\\start_video"), "start", self)
         play_video.triggered.connect(self.play_video)
         tool.addAction(play_video)
 
@@ -66,11 +67,15 @@ class VideoDisplay(QMainWindow):
     def play_video(self):
         if self.file:
             if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+                self.sender().setIcon(QIcon(".\\.\\image\\start_video"))
+                self.sender().setText("start")
                 self.mediaPlayer.pause()
             else:
+                self.sender().setIcon(QIcon(".\\.\\image\\pause_video"))
+                self.sender().setText("pause")
                 self.mediaPlayer.play()
         else:
-            QMessageBox.warning(self, "Warning", "The file path is invalid!")
+            QMessageBox.warning(self, "No Video Error", "Please load video first!", QMessageBox.Ok)
 
     def ok(self):
         self.apply()
@@ -79,10 +84,13 @@ class VideoDisplay(QMainWindow):
     def apply(self):
         self.getPro()
         if self.file:
+            print(self.file)
+            self.setCentralWidget(self.videoWidget)
             if QFileInfo(self.file).isFile():
                 try:
                     self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.file)))
-                    self.mediaPlayer.setPosition(self.strTime2intTime(self.startPos))
+                    self.mediaPlayer.setPosition(self.getStartTime
+                        (self.startPos))
                 except Exception as e:
                     print(e)
             else:
@@ -91,27 +99,26 @@ class VideoDisplay(QMainWindow):
         # 发送信号
         self.propertiesChanged.emit(self.getInfo())
 
-
     def getPro(self):
         self.file = self.pro.tab1.file_name.text()
         self.startPos = self.pro.tab1.startPos.text()
         self.endPos = self.pro.tab1.endPos.text()
         self.back_color = self.pro.tab1.back_color.currentText()
         self.transparent_value = self.pro.tab1.transparent.value()
-        isStopText = self.pro.tab1.stop_after.currentText()
-        if isStopText == "Yes":
-            self.stop_after = True
-        else:
-            self.stop_after = False
-        self.stop_after_mode = self.pro.tab1.stop_after_mode.currentText()
+        # isStopText = self.pro.tab1.stop_after.currentText()
+        # if isStopText == "Yes":
+        #     self.stop_after = True
+        # else:
+        #     self.stop_after = False
+        # self.stop_after_mode = self.pro.tab1.stop_after_mode.currentText()
         isStretchText = self.pro.tab1.stretch.currentText()
         if isStretchText == "Yes":
             self.stretch = True
         else:
             self.stretch = False
         self.stretch_mode = self.pro.tab1.stretch_mode.currentText()
-        self.end_video_action = self.pro.tab1.end_video_action.currentText()
-        self.display_name = self.pro.tab1.display_name.currentText()
+        # self.end_video_action = self.pro.tab1.end_video_action.currentText()
+        self.screen_name = self.pro.tab1.display_name.currentText()
         isClearText = self.pro.tab1.clear_after.currentText()
         if isClearText == "Yes":
             self.clear_after = True
@@ -123,7 +130,7 @@ class VideoDisplay(QMainWindow):
         self.h_size = self.pro.tab2.height.currentText()
 
     @staticmethod
-    def strTime2intTime(str_time):
+    def getStartTime(str_time):
         try:
             h = int(str_time[0:2])
             m = int(str_time[3:5])
@@ -144,12 +151,12 @@ class VideoDisplay(QMainWindow):
             "end position": self.endPos,
             "back color": self.back_color,
             "transparent": self.transparent_value,
-            "stop after": self.stop_after,
-            "stop after mode": self.stop_after_mode,
+            # "stop after": self.stop_after,
+            # "stop after mode": self.stop_after_mode,
             "stretch": self.stretch,
             "stretch mode": self.stretch_mode,
-            "end video mode": self.end_video_action,
-            "display name": self.display_name,
+            # "end video mode": self.end_video_action,
+            "screen name": self.screen_name,
             "X position": self.x_pos,
             "Y position": self.y_pos,
             "width": self.w_size,
