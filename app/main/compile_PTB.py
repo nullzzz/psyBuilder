dd0444bad4f05556cc4932e661c4a18773ff3c44
Administrator <zhangyang873@gmail.com>
2019-07-21 16:24:54 +0800

    TBC

--- app/main/compile_PTB.py
+++ app/main/compile_PTB.py
@@ -3,7 +3,6 @@ import sys
 import datetime
 import re
 
-
 from PyQt5.QtCore import Qt, QSettings, QTimer, QPropertyAnimation
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QAction, QApplication, QFileDialog, QMessageBox
