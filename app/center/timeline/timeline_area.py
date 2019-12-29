c2e579f3e8b9ec7640ee848c377ade5d7524899b
liyang52520 <378776683@qq.com>
2019-12-29 20:08:18 +0800

    init frame.

--- app/center/timeline/timeline_area.py
+++ app/center/timeline/timeline_area.py
@@ -1,12 +1,54 @@
-from PyQt5.QtWidgets import QMainWindow, QGraphicsView, QGraphicsScene
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsWidget, QGraphicsAnchorLayout
 
 from app.info import Info
 from .timeline_item_widget import TimelineItemWidget
 
 
-class TimelineView(QGraphicsView):
+class TimelineScene(QGraphicsScene):
     def __init__(self, scene: QGraphicsScene):
-        super(TimelineView, self).__init__(scene)
+        super(TimelineScene, self).__init__(scene)
+        # set container to set layout
+        container = QGraphicsWidget(None, Qt.Widget)
+        container.setPos(0, 0)
+        # set container's layout
+        layout = QGraphicsAnchorLayout()
+        container.setLayout(layout)
+        # add container to scene
+        self.addItem(container)
+
+
+class TimelineArea(QGraphicsView):
+    """
+
+    """
+
+    def __init__(self, parent=None):
+        super(TimelineArea, self).__init__(parent)
+
+        # scene is the real place to put items
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+        # view is a widget to contain scene
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+
+    def addItem(self, widget_type: int = None, widget_id: int = None) -> None:
+        """
+        add timeline item into timeline
+        :param widget_type: item add_type.
+        :param widget_id: if id is provided, we don't need to create new one.
+        :return:
+        """
+        # create new item
+        timeline_item = TimelineItemWidget(widget_type=widget_type, widget_id=widget_id)
+        # todo add it into scene
+
+    def itemCount(self) -> int:
+        """
+        count of items
+        :return:
+        """
+        return self.item_count
 
     def dragEnterEvent(self, e):
         """
@@ -37,40 +79,3 @@ class TimelineView(QGraphicsView):
         drag icon leave this widget, ignore this drag
         """
         e.ignore()
-
-
-class TimelineArea(QMainWindow):
-    """
-
-    """
-
-    def __init__(self, parent=None):
-        super(TimelineArea, self).__init__(parent)
-
-        # scene is the real place to put items
-        self.scene = QGraphicsScene()
-        # view is a widget to contain scene
-        self.view = TimelineView(self.scene)
-        self.view.setDragMode(QGraphicsView.ScrollHandDrag)
-        # embed scene into this widget.
-        self.setCentralWidget(self.view)
-        # set it can drop
-        self.setAcceptDrops(True)
-
-    def addItem(self, widget_type: int = None, widget_id: int = None) -> None:
-        """
-        add timeline item into timeline
-        :param widget_type: item add_type.
-        :param widget_id: if id is provided, we don't need to create new one.
-        :return:
-        """
-        # create new item
-        timeline_item = TimelineItemWidget(widget_type=widget_type, widget_id=widget_id)
-        # todo add it into scene
-
-    def itemCount(self) -> int:
-        """
-        count of items
-        :return:
-        """
-        return self.item_count
