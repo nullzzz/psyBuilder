db1f79653551055b01397d1718d90332f9cbbd23
liyang52520 <378776683@qq.com>
2019-12-30 20:40:24 +0800

    init frame.

--- app/center/timeline/timeline_area.py
+++ app/center/timeline/timeline_area.py
@@ -2,20 +2,37 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsWidget, QGraphicsAnchorLayout
 
 from app.info import Info
-from .timeline_item_widget import TimelineItemWidget
+from .timeline_item import TimelineItem
 
 
 class TimelineScene(QGraphicsScene):
-    def __init__(self, scene: QGraphicsScene):
-        super(TimelineScene, self).__init__(scene)
+    def __init__(self):
+        super(TimelineScene, self).__init__(None)
+        # data
+        self.item_count = 0
         # set container to set layout
-        container = QGraphicsWidget(None, Qt.Widget)
-        container.setPos(0, 0)
+        self.container = QGraphicsWidget(None, Qt.Widget)
+        self.container.setPos(0, 0)
         # set container's layout
-        layout = QGraphicsAnchorLayout()
-        container.setLayout(layout)
+        self.layout = QGraphicsAnchorLayout()
+        self.container.setLayout(self.layout)
         # add container to scene
-        self.addItem(container)
+        self.addItem(self.container)
+
+    def addTimelineItem(self, item: TimelineItem, index: int) -> None:
+        """
+        add timeline item in this scene
+        :param item:
+        :param index: the index of item which starts from 0
+        :return:
+        """
+
+    def deleteTimelineItem(self, index: int) -> None:
+        """
+        delete timeline item in this scene
+        :param index: the index of item which starts from 0
+        :return:
+        """
 
 
 class TimelineArea(QGraphicsView):
@@ -27,28 +44,37 @@ class TimelineArea(QGraphicsView):
         super(TimelineArea, self).__init__(parent)
 
         # scene is the real place to put items
-        self.scene = QGraphicsScene()
+        self.scene = TimelineScene()
         self.setScene(self.scene)
         # view is a widget to contain scene
         self.setDragMode(QGraphicsView.ScrollHandDrag)
 
-    def addItem(self, widget_type: int = None, widget_id: int = None) -> None:
+    def addTimelineItem(self, widget_type: int = None, widget_id: int = None, index: int = 0) -> None:
         """
         add timeline item into timeline
         :param widget_type: item add_type.
         :param widget_id: if id is provided, we don't need to create new one.
+        :param index: index of item
         :return:
         """
         # create new item
-        timeline_item = TimelineItemWidget(widget_type=widget_type, widget_id=widget_id)
+        timeline_item = TimelineItem(widget_type=widget_type, widget_id=widget_id)
         # todo add it into scene
+        self.scene.addTimelineItem(timeline_item, index)
+
+    def deleteTimelineItem(self, index: int):
+        """
+        delete in this area
+        :param index:
+        :return:
+        """
 
     def itemCount(self) -> int:
         """
         count of items
         :return:
         """
-        return self.item_count
+        return self.scene.item_count
 
     def dragEnterEvent(self, e):
         """
--- app/center/timeline/timeline_item.py
+++ app/center/timeline/timeline_item.py
@@ -1,10 +1,10 @@
-from PyQt5.QtWidgets import QGraphicsPixmapItem
+from PyQt5.QtWidgets import QGraphicsPixmapItem, QGraphicsLayoutItem
 
 from app.func import Func
 from app.info import Info
 
 
-class TimelineItemWidget(QGraphicsPixmapItem):
+class TimelineItem(QGraphicsLayoutItem):
     """
     it is widget_type item in timeline.
     """
@@ -17,7 +17,7 @@ class TimelineItemWidget(QGraphicsPixmapItem):
         :param widget_id: if widget_id has provided, we don't need to generate a new widget_id
         :param widget_name: like widget_id above.
         """
-        super(TimelineItemWidget, self).__init__(parent)
+        super(TimelineItem, self).__init__(parent)
         # if widget_id/widget_name has provided
         self.widget_id = widget_id
         self.widget_name = widget_name
@@ -26,5 +26,7 @@ class TimelineItemWidget(QGraphicsPixmapItem):
             self.widget_id = Func.generateWidgetId(widget_type)
         if not self.widget_name:
             self.widget_name = Func.generateWidgetName(widget_type)
-        # select its widget_type according to its widget add_type.
-        self.setPixmap(Func.getImage(f"widgets/{Info.WidgetType[widget_type]}"))
+        # select its widget_type according to its widget type.
+        self.pixmap_item = QGraphicsPixmapItem()
+        self.pixmap_item.setPixmap(Func.getImage(f"widgets/{Info.WidgetType[widget_type]}"))
+        self.setGraphicsItem(self.pixmap_item)
