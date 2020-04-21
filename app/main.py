5f80077902dfc306914e03b0341973f783d35042
Administrator <zhangyang873@gmail.com>
2020-04-22 00:11:12 +0800

    1) committed pyperclip ... 2) added psybuilder.ico
    3) fixed a bug in control.py line 25-27 : removed _
    
    4) replace all getcwd with os.path.dirname(os.path.abspath(__file__)) to compatible with pyinstaller

--- app/main.py
+++ app/main.py
@@ -678,7 +678,8 @@ class Psy(QMainWindow):
         # choose directory
         file_directory = Info.FILE_DIRECTORY
         if not file_directory:
-            file_directory = os.getcwd()
+            # file_directory = os.getcwd()
+            file_directory = os.path.dirname(os.path.abspath(__file__))
         # get new file's directory
         file_directory = QFileDialog().getExistingDirectory(None, "Choose Directory", file_directory,
                                                             QFileDialog.ShowDirsOnly)
@@ -700,7 +701,8 @@ class Psy(QMainWindow):
         else:
             file_directory = Info.FILE_DIRECTORY
             if not file_directory:
-                file_directory = os.getcwd()
+                # file_directory = os.getcwd()
+                file_directory = os.path.dirname(os.path.abspath(__file__))
             file_path, _ = QFileDialog().getSaveFileName(self, "Save file", file_directory, "Psy Files (*.psy);")
             if file_path:
                 if not re.search(r"\.psy$", file_path):
@@ -730,7 +732,8 @@ class Psy(QMainWindow):
         """
         directory = Info.FILE_DIRECTORY
         if not directory:
-            directory = os.getcwd()
+            # directory = os.getcwd()
+            directory = os.path.dirname(os.path.abspath(__file__))
         file_path, _ = QFileDialog().getSaveFileName(self, "Save As file", directory, "Psy Files (*.psy);")
         if file_path:
             if not re.search(r"(\.psy|\.ini)$", file_path):
@@ -751,7 +754,7 @@ class Psy(QMainWindow):
         """
         open file through restart software
         """
-        file_path, _ = QFileDialog().getOpenFileName(self, "Choose file", os.getcwd(), "Psy File (*.psy)")
+        file_path, _ = QFileDialog().getOpenFileName(self, "Choose file", os.path.dirname(os.path.abspath(__file__)), "Psy File (*.psy)")
         if file_path and file_path != Info.FILE_NAME:
             # store current state
             self.store(Info.TempFile, False)
