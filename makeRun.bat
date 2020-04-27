fdf0b91f3b941d7b45e67d41c5c46004488751ba
Administrator <zhangyang873@gmail.com>
2020-04-27 16:03:43 +0800

    added a batch file to compile the project for windows

---
+++ makeRun.bat
@@ -0,0 +1,11 @@
+@echo off
+cd  %~dp0
+rd /S/Q .\build
+rd /S/Q .\dist
+
+pyinstaller -i .\source\images\common\psybuilder.ico -w --noconfirm --k=asdgaFgWE1e42a --hidden-import pkg_resources.py2_warn run.py
+
+
+echo on
+
+pause
