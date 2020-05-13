503efb1b387f8f48413c69ffafedf13011b3517d
Yang Zhang <zhangyang873@gmail.com>
2020-05-13 23:04:43 +0800

    compile the project under mac: half done

--- makeRunMac.sh
+++ makeRunMac.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+work_path=$(dirname $0)
+cd ${work_path}
 
 sudo pyinstaller -i ./source/images/common/icon.icns -w --clean --add-data source:source --add-data yanglabMFuns:yanglabMFuns --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' --noconfirm --k=as2gaFgWE1weA2a --hidden-import pkg_resources.py2_warn run.py
 
 echo "start to codesign the app"
 
-APP_PATH="/Users/Zy/PycharmProjects/ptbGui/dist/run.app"
+APP_PATH="./dist/run.app"
 sudo codesign --deep -f -s B1B17CC1866C978FE90BA83228B6D4045A1081A8  "$APP_PATH"
