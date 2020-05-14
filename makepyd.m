b3ddb0ee86aec64776748ff85a77b29d6a4ce90a
Administrator <zhangyang873@gmail.com>
2020-05-14 15:42:54 +0800

    bugfix

--- makepyd.m
+++ makepyd.m
@@ -1,27 +1,32 @@
-function makepyd(clearHisOnly)
+function makepyd(clearHisOnly,runAllMatchFiles)
 
 if ~exist('clearHisOnly','var')
     clearHisOnly = false;
 end
 
 
-directory = fullfile(pwd,'ptbGui');
+if ~exist('runAllMatchFiles','var')
+    runAllMatchFiles = true;
+end
+
+
+directory = fileparts(mfilename('fullpath'));
 deleteFiles(directory,'*.pyd');
 deleteFiles(directory,'*.c');
 
 if ~clearHisOnly
-    untouchedFiles = {fullfile(directory,'run.py'),fullfile(directory,'doNothing.py'),fullfile(directory,'app\center\events\slider\item\textItem.py')};
+    untouchedFiles = {mkFullFile(directory,'run.py'),mkFullFile(directory,'doNothing.py'),mkFullFile(directory,'app\center\events\slider\item\textItem.py')};
     
-    touchedFiles = {fullfile(directory,'app\menubar\compile_PTB.py')};
+    touchedFiles = {mkFullFile(directory,'app\menubar\compile_PTB.py')};
     
-    files= dirall(directory,'\*.py',[],{'__init__.py'});
+    files= dirall(directory,'*.py',[],{'__init__.py'});
     failedFileIdx = [];
     errorResults = {};
     iError = 1;
     for iFile = 1:numel(files)
         
-                
-        if ~ismember(files(iFile).name,untouchedFiles) && ismember(files(iFile).name,touchedFiles)
+        
+        if ~ismember(files(iFile).name,untouchedFiles) && (runAllMatchFiles||ismember(files(iFile).name,touchedFiles))
             
             [status,result] = system(['cythonize -i -3 ',files(iFile).name]);
             [cpath,cfilename,cSuffix] = fileparts(files(iFile).name);
@@ -34,14 +39,28 @@ if ~clearHisOnly
                 errorResults{iError} = result;
                 iError = iError +1;
             else
-                [status,result] = system(['ren "',fullfile(cpath,[cfilename,'.cp38-win_amd64.pyd']),'" ',cfilename,'.pyd']);
+                if IsWin
+                    [status,result] = system(['ren "',fullfile(cpath,[cfilename,'.cp38-win_amd64.pyd']),'" ',cfilename,'.pyd']);
+                end
             end
-            [status,result] = system(['del /F/S/Q "',fullfile(cpath,[cfilename,'.c']),'"']);
+            if IsWin
+                [status,result] = system(['del /F/S/Q "',fullfile(cpath,[cfilename,'.c']),'"']);
+            else
+                [status,result] = system(['rm -rf "',fullfile(cpath,[cfilename,'.c']),'"']);
+            end
         end
     end
     
     disp(files(failedFileIdx));
     
+end
+
+end
+
+function fullFileName = mkFullFile(directory,filename)
+
+temp = regexp(filename,'[\\/]','split');
+fullFileName = fullfile(directory,temp{:});
 end
 
 
